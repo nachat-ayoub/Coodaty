0e5832064be72b08378463861c12836e30d3659c
venomVxV <venomx2020x@gmail.com>
2021-03-30 15:31:49 +0100

    nothing much just cleaning stuff...

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Coodaty
-      <Description/>
+<Description />
 * Coodaty is a Html, Css and javascript Editor
-  This Editor is recommended for Arabic
-  community but anyone can use it...
+This Editor is recommended for Arabic
+community but anyone can use it...
 
-      <futures/>
+<Futures />
 * adding the js console
 * adding a special place to past the libraries links
 * adding multiple themes
-* redesign the UI & UX
+* redesign the UI & UX (1/3)
 * adding the choice of the template the user goona work with like react, Vue.js or angular...
 * storing the files and the ability to upload photos and files
 * any other futures just tell me to add it here!...
--- app.js
+++ app.js
@@ -1,61 +1,10 @@
-/*
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-  <header id="header">
-    <h2>Title</h2>
-    <button id="btn">click</button>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi.</p>
-  </header>
-</body>
-</html>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-  ${htmlval}
-</body>
-</html>
-
-
-
-
-
-
-
-
-
-var btn2 = document.getElementById("#btn2");
-var header = document.getElementById("#header");
-
-btn2.addEventListener('click', function(){
-   header.classList.toggle("bg") ;
-})
-
-
-*/
-var btn2 = document.getElementById("btn2")
-
+// Here is the variables area
 const playBTN = document.querySelector("#playBtn");
 const htmlBTN = document.querySelector("#htmlBtn");
 const cssBTN = document.querySelector("#cssBtn");
 const jsBTN = document.querySelector("#jsBtn");
-
 const result = document.querySelector("#result");
+
 // AREA CODE
 const previewAREA = document.querySelector(".preview-area");
 const htmlCODE = document.querySelector(".html-container");
@@ -63,7 +12,7 @@ const cssCODE = document.querySelector(".css-container");
 const jsCODE = document.querySelector(".js-container");
 
 
-// CSS onClick function
+// CSS btn onClick function
 
 cssBTN.addEventListener('click', function() {
   htmlCODE.classList.add('hide');
@@ -72,7 +21,7 @@ cssBTN.addEventListener('click', function() {
   cssCODE.classList.remove('hide');
 })
 
-// JS onClick function
+// JS btn onClick function
 
 jsBTN.addEventListener('click', function() {
   htmlCODE.classList.add('hide');
@@ -81,7 +30,7 @@ jsBTN.addEventListener('click', function() {
   jsCODE.classList.remove('hide');
 })
 
-// HTML onClick function
+// HTML btn onClick function
 
 htmlBTN.addEventListener('click', function() {
   jsCODE.classList.add('hide');
@@ -90,7 +39,7 @@ htmlBTN.addEventListener('click', function() {
   htmlCODE.classList.remove('hide');
 })
 
-// PLAY onClick function
+// PLAY btn onClick function
 
 playBTN.addEventListener('click', function() {
   jsCODE.classList.add('hide');
